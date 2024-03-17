412a7a13b488dd2cdd322d000ef647a2469eb8e3
Alexandru Nedelcu <160555764+alexn55@users.noreply.github.com>
2024-03-17 21:13:07 +0200

    Add files via upload

---
+++ x&0/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace xsi0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.label1.Location = new System.Drawing.Point(59, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player 1:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.label2.Location = new System.Drawing.Point(470, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Player 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.label3.Location = new System.Drawing.Point(136, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.label4.Location = new System.Drawing.Point(547, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Crimson;
+            this.button1.Location = new System.Drawing.Point(636, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "START JOC!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MingLiU-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.button2.Location = new System.Drawing.Point(636, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Renunta";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jocul X si 0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ x&0/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xsi0
+{
+    public partial class Form1 : Form
+    {
+        int x, a, b;
+        Button[,] matr = new Button[3, 3];
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+        }
+        void init()
+        {
+            x = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    matr[i, j].Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        void verifica()
+        {
+            if (matr[0, 0].Text == matr[1, 0].Text && matr[1, 0].Text == matr[2, 0].Text)
+                if (matr[0, 0].Text == "X")
+                {
+                    a++;
+                    label3.Text = a.ToString();
+                    MessageBox.Show("Castiga player 1");
+                    init();
+                }
+                else if (matr[0, 0].Text == "0")
+                {
+                    b++;
+                    label4.Text = b.ToString();
+                    MessageBox.Show("Castiga player 2");
+                    init();
+                }
+            if (matr[0, 1].Text == matr[1, 1].Text && matr[1, 1].Text == matr[2, 1].Text)
+                if (matr[0, 1].Text == "X")
+                {
+                    a++;
+                    label3.Text = a.ToString();
+                    MessageBox.Show("Castiga player 1");
+                    init();
+                }
+                else if (matr[0, 1].Text == "0")
+                {
+                    b++;
+                    label4.Text = b.ToString();
+                    MessageBox.Show("Castiga player 2");
+                    init();
+                }
+            if (matr[0, 2].Text == matr[1, 2].Text && matr[1, 2].Text == matr[2, 2].Text)
+                if (matr[0, 2].Text == "X")
+                {
+                    a++;
+                    label3.Text = a.ToString();
+                    MessageBox.Show("Castiga player 1");
+                    init();
+                }
+                else if (matr[0, 2].Text == "0")
+                {
+                    b++;
+                    label4.Text = b.ToString();
+                    MessageBox.Show("Castiga player 2");
+                    init();
+                }
+            if (matr[0, 0].Text == matr[0, 1].Text && matr[0, 1].Text == matr[0, 2].Text)
+                if (matr[0, 0].Text == "X")
+                {
+                    a++;
+                    label3.Text = a.ToString();
+                    MessageBox.Show("Castiga player 1");
+                    init();
+                }
+                else if (matr[0, 0].Text == "0")
+                {
+                    b++;
+                    label4.Text = b.ToString();
+                    MessageBox.Show("Castiga player 2");
+                    init();
+                }
+            if (matr[1, 0].Text == matr[1, 1].Text && matr[1, 1].Text == matr[1, 2].Text)
+                if (matr[1, 0].Text == "X")
+                {
+                    a++;
+                    label3.Text = a.ToString();
+                    MessageBox.Show("Castiga player 1");
+                    init();
+                }
+                else if (matr[1, 0].Text == "0")
+                {
+                    b++;
+                    label4.Text = b.ToString();
+                    MessageBox.Show("Castiga player 2");
+                    init();
+                }
+            if (matr[2, 0].Text == matr[2, 1].Text && matr[2, 1].Text == matr[2, 2].Text)
+                if (matr[2, 0].Text == "X")
+                {
+                    a++;
+                    label3.Text = a.ToString();
+                    MessageBox.Show("Castiga player 1");
+                    init();
+                }
+                else if (matr[2, 0].Text == "0")
+                {
+                    b++;
+                    label4.Text = b.ToString();
+                    MessageBox.Show("Castiga player 2");
+                    init();
+                }
+            if (matr[0, 0].Text == matr[1, 1].Text && matr[1, 1].Text == matr[2, 2].Text)
+                if (matr[0, 0].Text == "X")
+                {
+                    a++;
+                    label3.Text = a.ToString();
+                    MessageBox.Show("Castiga player 1");
+                    init();
+                }
+                else if (matr[0, 0].Text == "0")
+                {
+                    b++;
+                    label4.Text = b.ToString();
+                    MessageBox.Show("Castiga player 2");
+                    init();
+                }
+            if (matr[2, 0].Text == matr[1, 1].Text && matr[1, 1].Text == matr[0, 2].Text)
+                if (matr[2, 0].Text == "X")
+                {
+                    a++;
+                    label3.Text = a.ToString();
+                    MessageBox.Show("Castiga player 1");
+                    init();
+                }
+                else if (matr[2, 0].Text == "0")
+                {
+                    b++;
+                    label4.Text = b.ToString();
+                    MessageBox.Show("Castiga player 2");
+                    init();
+                }
+        }
+        private void matr_Click(object sender, EventArgs e)
+        {
+            string s = ((Button)sender).Name.ToString();
+            bool sem = false;
+            if (s.Equals("matr00"))
+            {
+                sem = true;
+                if (x == 1)
+                    matr[0, 0].Text = "X";
+                else
+                    matr[0, 0].Text = "0";
+            }
+            else
+                if (s.Equals("matr10"))
+            {
+                if (matr[1, 0].Text == "")
+                {
+                    sem = true;
+                    if (x == 1)
+                        matr[1, 0].Text = "X";
+                    else
+                        matr[1, 0].Text = "0";
+                }
+            }
+            else
+                if (s.Equals("matr20"))
+            {
+                if (matr[2, 0].Text == "")
+                {
+                    sem = true;
+                    if (x == 1) matr[2, 0].Text = "X";
+                    else matr[2, 0].Text = "0";
+                }
+            }
+            else
+            if (s.Equals("matr01"))
+            {
+                if (matr[0, 1].Text == "")
+                {
+                    sem = true;
+                    if (x == 1)
+                        matr[0, 1].Text = "X";
+                    else matr[0, 1].Text = "0";
+                }
+            }
+            else
+        if (s.Equals("matr11"))
+            {
+                if (matr[1, 1].Text == "")
+                {
+                    sem = true;
+                    if (x == 1) matr[1, 1].Text = "X";
+                    else matr[1, 1].Text = "0";
+                }
+            }
+            else
+                if (s.Equals("matr21"))
+            {
+                if (matr[2, 1].Text == "")
+                {
+                    sem = true;
+                    if (x == 1) matr[2, 1].Text = "X";
+                    else matr[2, 1].Text = "0";
+                }
+            }
+            else
+                if (s.Equals("matr02"))
+            {
+                if (matr[0, 2].Text == "")
+                {
+                    sem = true;
+                    if (x == 1) matr[0, 2].Text = "X";
+                    else matr[0, 2].Text = "0";
+                }
+            }
+            else
+                if (s.Equals("matr12"))
+            {
+                if (matr[1, 2].Text == "")
+                {
+                    sem = true;
+                    if (x == 1) matr[1, 2].Text = "X";
+                    else matr[1, 2].Text = "0";
+                }
+            }
+            else
+                if (s.Equals("matr22"))
+            {
+                if (matr[2, 2].Text == "")
+                {
+                    sem = true;
+                    if (x == 1) matr[2, 2].Text = "X";
+                    else matr[2, 2].Text = "0";
+                }
+            }
+            if (sem == true)
+            {
+                x = x * (-1);
+                verifica();
+            }
+        }
+    
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                x = 1;
+                a = 0;
+                b = 0;
+                label1.Visible = true;
+                label2.Visible = true;
+                label3.Visible = true;
+                label4.Visible = true;
+                label3.Text = "0";
+                label4.Text = "0";
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        matr[i, j] = new Button();
+                        matr[i, j].Parent = this;
+                        matr[i, j].Left = i * 70 + 70;
+                        matr[i, j].Top = j * 70 + 70;
+                        matr[i, j].Width = 70;
+                        matr[i, j].Height = 70;
+                        matr[i, j].BackColor = System.Drawing.Color.DarkCyan;
+                        matr[i, j].FlatStyle = FlatStyle.Flat;
+                        matr[i, j].FlatAppearance.BorderSize = 3;
+                        matr[i, j].FlatAppearance.BorderColor = Color.Pink;
+                        matr[i, j].ForeColor = System.Drawing.Color.Cyan;
+                        matr[i, j].Font= new Font("Arial Black",24)  ;
+                        matr[i, j].Text = "";
+                        matr[i, j].Visible = true;
+                        matr[i, j].Name = "matr" + i.ToString() + j.ToString();
+                        matr[i, j].Click += new EventHandler(matr_Click);
+                    }
+                    button1.Enabled = false;
+                }
+            }
+        }
+    }
+
